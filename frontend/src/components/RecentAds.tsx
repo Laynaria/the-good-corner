df84bc07f8c866876a33b965aa6971a22dc28cf4
Laynaria <pro.laynaria@outlook.fr>
2023-09-26 11:55:27 +0200

    routing done

--- frontend/src/components/RecentAds.tsx
+++ frontend/src/components/RecentAds.tsx
@@ -6,37 +6,37 @@ const RecentAds = () => {
       title: "Table",
       price: 120,
       imgUrl: "/images/table.webp",
-      link: "/ads/table",
+      link: "/ad/table",
     },
     {
       title: "Dame-jeanne",
       price: 75,
       imgUrl: "/images/dame-jeanne.webp",
-      link: "/ads/dame-jeanne",
+      link: "/ad/dame-jeanne",
     },
     {
       title: "Vide-poche",
       price: 4,
       imgUrl: "/images/vide-poche.webp",
-      link: "/ads/vide-poche",
+      link: "/ad/vide-poche",
     },
     {
       title: "Vaisselier",
       price: 900,
       imgUrl: "/images/vaisselier.webp",
-      link: "/ads/vaisseliere",
+      link: "/ad/vaisseliere",
     },
     {
       title: "Bougie",
       price: 8,
       imgUrl: "/images/bougie.webp",
-      link: "/ads/bougie",
+      link: "/ad/bougie",
     },
     {
       title: "Porte-magazine",
       price: 45,
       imgUrl: "/images/porte-magazine.webp",
-      link: "/ads/porte-magazine",
+      link: "/ad/porte-magazine",
     },
   ];
   return (
---
+++ frontend/src/pages/about.tsx
@@ -0,0 +1,5 @@
+const about = () => {
+  return <p>This project was made with Wild Code School to learn Next JS.</p>;
+};
+
+export default about;
