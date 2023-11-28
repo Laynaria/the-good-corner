0522e46d7899d8cb03b4418c63ca9a47a3ed9e0b
Laynaria <pro.laynaria@outlook.fr>
2023-11-28 15:04:41 +0100

    tried a change on frontend to see if container updates when updating a file, and success.

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -1,5 +1,10 @@
 const about = () => {
-  return <p>This project was made with Wild Code School to learn Next JS.</p>;
+  return (
+    <p>
+      This project was made with Wild Code School to learn Next JS. It is now
+      Dockerized!
+    </p>
+  );
 };
 
 export default about;
